f92e96bd744ea42febc9e6679facf30e537a5c82
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2020-04-21 15:29:25 +0530

    Converted all config dotfiles into visible files

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: {
+    'postcss-nested': {},
+    'postcss-insert': {},
+    'postcss-implicit-var': {},
+    'postcss-arithmetic': {},
+    'postcss-font-magician': {},
+  }
+}
