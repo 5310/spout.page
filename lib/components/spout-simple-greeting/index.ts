622e266df223594453f4a348aff02e3fdffb5ed7
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2020-04-22 19:11:52 +0530

    Refactored code to remove src/

---
+++ lib/components/spout-simple-greeting/index.ts
@@ -0,0 +1,16 @@
+import { LitElement, html, css, property, customElement } from '/web_modules/lit-element.js'
+
+@customElement('spout-simple-greeting')
+export class SpoutSimpleGreeting extends LitElement {
+
+  @property()
+  name = 'World'
+
+  render() {
+    return html`
+      <link rel="stylesheet" href="/lib/components/spout-simple-greeting/index.css" />
+      <p>Hullo, ${this.name}!!</p>
+    `
+  }
+
+}
