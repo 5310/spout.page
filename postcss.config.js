619d99d25ca075d4becd23eeac3bd041b9fb9b1d
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2020-04-24 17:54:38 +0530

    Added CSS4 media query polyfill and a prettifier

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,8 @@ module.exports = {
     'postcss-insert': {},
     'postcss-implicit-var': {},
     'postcss-arithmetic': {},
+    'postcss-media-minmax': {},
     'postcss-font-magician': {},
+    'postcss-prettify': {},
   }
 }
