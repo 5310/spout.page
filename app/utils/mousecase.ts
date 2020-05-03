22d521e079c1c1a8b5421592fb4d1ccb560a0b3d
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2020-05-03 18:29:29 +0530

    ported yowainwright/mousecase locally Because it has no exported types and can't accept elements instead of a selector

---
+++ app/utils/mousecase.ts
@@ -0,0 +1,115 @@
+// source: https://github.com/yowainwright/mousecase
+// Minimal changes.
+//
+// This is ported locally because the original package publishes no type annotations despite being TypeScript
+// and despite claiming to in the doc, cannot accept an HTMLElement as target, only a string selector
+// which will not work inside a web-component
+
+interface MousecasePropArguments {
+  cssClass?: string
+  rule?: boolean
+}
+
+interface MousecaseProps {
+  activeClass: string
+  cssClass: string
+  el: HTMLElement
+  rule: boolean
+}
+
+interface MousecaseState {
+  isDown: boolean
+  startx: number
+  scrollLeft: number
+  isOn: boolean
+}
+
+type MousecaseThis = any
+
+interface MousecaseResult {
+  props: MousecaseProps
+  state: MousecaseState
+  canUseMousecase: (target: HTMLElement, rule: boolean) => boolean
+  mouseMove: (e: MouseEvent) => MousecaseThis
+  mouseDown: (e: MouseEvent) => MousecaseThis
+  mouseNotDown: () => MousecaseThis
+  manageState: () => MousecaseThis
+  init: () => void
+  off: () => MousecaseThis
+  on: () => MousecaseThis
+}
+
+const mousecase = (
+  target: HTMLElement,
+  {
+    cssClass = 'js-mousecase',
+    rule = true,
+  }: MousecasePropArguments = {}
+): MousecaseResult => ({
+  props: {
+    el: target,
+    cssClass,
+    rule,
+    activeClass: `${cssClass}--is-active`,
+  },
+  state: {
+    isDown: false,
+    startx: 0,
+    scrollLeft: 0,
+    isOn: false,
+  },
+  canUseMousecase(target_: HTMLElement, rule_: boolean): boolean {
+    if (
+      !target_ ||
+      rule_ === false
+    ) return false
+    return true
+  },
+  mouseMove(e: MouseEvent) {
+    if (!this.state.isDown) return
+    e.preventDefault()
+    const { el } = this.props
+    const initial = e.pageX - el.offsetLeft
+    const distance = (initial - this.state.startx) * 3
+    el.scrollLeft = this.state.scrollLeft - distance
+    return this
+  },
+  mouseDown(e: MouseEvent) {
+    const { activeClass, el } = this.props
+    this.state.isDown = true
+    el.classList.add(activeClass)
+    this.state.startx = e.pageX - el.offsetLeft
+    this.state.scrollLeft = el.scrollLeft
+    return this
+  },
+  mouseNotDown() {
+    this.state.isDown = false
+    const { activeClass, el } = this.props
+    el.classList.remove(activeClass)
+    return this
+  },
+  manageState() {
+    if (!this.state.isOn) return
+    const { el } = this.props
+    el.addEventListener('mousemove', (e) => this.mouseMove(e))
+    el.addEventListener('mousedown', (e) => this.mouseDown(e))
+    el.addEventListener('mouseleave', () => this.mouseNotDown())
+    el.addEventListener('mouseup', () => this.mouseNotDown())
+    return this
+  },
+  init() {
+    if (!this.canUseMousecase(target, this.props.rule)) return
+    this.state.isOn = true
+    this.manageState()
+  },
+  off() {
+    this.state.isOn = false
+    return this
+  },
+  on() {
+    this.state.isOn = true
+    return this
+  },
+})
+
+export default mousecase
