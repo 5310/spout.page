c272352f1cd628a3fb574972f2e7564c7546462b
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2020-05-11 12:03:31 +0530

    made the logging flag global Sue me.

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -1,6 +1,4 @@
-const LOG = true
-
 export default (...args: any[]) => {
   // tslint:disable-next-line: no-console
-  if (LOG) console.log(...args)
+  if ((self as any).LOG) console.log(...args)
 }
